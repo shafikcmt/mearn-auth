571ebdb4431f855cae650ed04478cce9287a0ec2
shafikcmt <mdshafiqulislam822@gmail.com>
2024-11-29 09:25:35 +0530

    Work for create module

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,8 +5,11 @@ const app = express();
 const registerModel = require('./Models/Register');
 const postModel = require('./Models/Post');
 const bcrypt = require('bcrypt');
+const multer = require('multer');
+const bodyParser = require('body-parser');
 
 app.use(express.json());
+app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/register',(req,res)=>{
@@ -42,6 +45,27 @@ app.post('/login',(req,res)=>{
     })
     .catch(err => res.json(err))
 })
+
+const storage = multer.diskStorage({
+destination: (req,file,cb) => cb(null, '/uploads'),
+filename: (req,file,cb) => cb(null, Date.now()+'_'+file.originalname)
+})
+
+const uploads = multer({storage})
+
+app.post('/addPost',uploads.single('image'),(req,res)=>{
+  const {title,description} = req.body;
+  const imageUrl = `/uploads/${req.file.filename}`;
+
+  postModel.create({
+    title:title,
+    description: description,
+    imageUrl:imageUrl
+  })
+  .then(result => res.json(result))
+  .catch(err => res.json(err))
+})
+
 
 mongoose.connect('mongodb://localhost:27017/authentication');
 
--- FrontEnd/authentication/src/Components/AddBlog.jsx
+++ FrontEnd/authentication/src/Components/AddBlog.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function AddBlog() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState(null);
+    const [msg, setMsg] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append('title',title);
+        formData.append('description',description);
+        formData.append('image',image);
+
+        try{
+            const result = axios.post('http://localhost:3000/addPost/',formData,{
+                headers:{
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+            console.log(result.data)
+            setMsg('Blog Uploaded Successfully!')
+        }catch(err){
+            setMsg('Failed to Blog Upload!!')
+        }
+
+    }
   return (
     <>
         <div className="card">
@@ -8,20 +34,20 @@ function AddBlog() {
                 <h5 className="card-title">Add Post</h5>
             </div>
             <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Add Title</label>
-                    <input type="text" className="form-control" placeholder='Add Title' />
+                    <input type="text" className="form-control" placeholder='Add Title' onChange={(e) => setTitle(e.target.value)} />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Add Description</label>
-                   <textarea placeholder='Add Description' className='form-control'></textarea>
+                   <textarea placeholder='Add Description' className='form-control' onChange={(e) => setDescription(e.target.value)}></textarea>
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="image" className="form-label">Add Image</label>
-                    <input type="file" className="form-control" />
+                    <input type="file" className="form-control" name='image' onChange={(e) => setImage(e.target.files[0])} />
                 </div>
                
                
